ca1862288be0baa1e2c7bc716a64813f65ea3b0b
Wasit Faraizi <wasit.allin1@gmail.com>
2022-06-26 23:10:53 +0600

    added bit.ly api

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-const btn = document.getElementById('menu-btn')
+const apiToken = 'process.env.API_TOKEN'
+let currentLink
+
+const shortLink = document.getElementById('short-link-1')
+const shortLink2 = document.getElementById('short-link-2')
+const shortLink3 = document.getElementById('short-link-3')
+const originalLink = document.getElementById('long-link-1')
+const link2 = document.getElementById('long-link-2')
+const link3 = document.getElementById('long-link-3')
+
+const hamburgerBtn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 
 const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
-linkForm.addEventListener('submit', formSubmit)
-
-btn.addEventListener('click', navToggle)
+hamburgerBtn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
 
 // Toggle Mobile Menu
 function navToggle() {
-  btn.classList.toggle('open')
+  hamburgerBtn.classList.toggle('open')
   menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
 }
@@ -31,7 +39,7 @@ function validURL(str) {
   return !!pattern.test(str)
 }
 
-function formSubmit(e) {
+async function formSubmit(e) {
   e.preventDefault()
 
   if (input.value === '') {
@@ -43,6 +51,57 @@ function formSubmit(e) {
   } else {
     errMsg.innerHTML = ``
     input.classList.remove('border-red')
-    alert('Success')
+    let longLink
+
+    if (input.value.includes('https://')) {
+      longLink = input.value
+    } else {
+      longLink = `https://${input.value}`
+    }
+
+    const link = await fetchShortLink(longLink)
+
+    if (link !== 'error') {
+      errMsg.classList.add('text-green-500')
+      errMsg.classList.remove('text-red')
+      errMsg.innerHTML = `Copied!`
+      input.classList.add('border-green-500')
+
+      input.value = `${link}`
+
+      link3.innerHTML = link2.innerHTML
+      shortLink3.innerHTML = shortLink2.innerHTML
+      link2.innerHTML = originalLink.innerHTML
+      shortLink2.innerHTML = shortLink.innerHTML
+
+      originalLink.innerHTML = currentLink
+      shortLink.innerHTML = link
+    } else {
+      errMsg.innerHTML = 'Link is already shortened!'
+    }
+  }
+}
+
+async function fetchShortLink(longLink) {
+  currentLink = longLink
+  const res = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${apiToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'long_url': longLink,
+    }),
+  })
+
+  const data = await res.json()
+
+  if (!data.errors) {
+    const link = data.link.slice(8)
+
+    return link
+  } else {
+    return 'error'
   }
 }
